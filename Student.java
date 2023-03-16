29ca0f7126849385d4ff8f5edc3881fb033b741f
B153Prem <sirsikarp@gmail.com>
2023-03-16 20:29:42 +0530

    third commit

---
+++ Student.java
@@ -0,0 +1,7 @@
+public class Student
+{
+  public void m1
+ {
+    system.out.println("m1");
+ }
+}
---
+++ Xyz.java
@@ -0,0 +1,15 @@
+public class Xyz
+{
+   public void m1
+   {
+      syatem.out.println("m1");
+   }
+   public void m3
+   {
+     system.out.println("m3");
+   }
+
+
+
+
+}
