a02a3370c1b225b0f24e8d5f1ee925ee5c8ccf86
B153Prem <sirsikarp@gmail.com>
2023-03-18 13:40:30 +0530

    new file added

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person
+{
+    int a=10;
+    string s="Anuj";
+
+    public void m1()
+    {
+       System.out.println("Person--m1")
+    }
+     public static void main(String [] args)
+     {
+        Person p=new Peron();
+               p.m1();
+         System.out.println(p.a);
+         System.out.println(p.s);
+   
+     }
+}
